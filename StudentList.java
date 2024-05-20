12e779683684453db99eabfef64e881e06303caa
Shuvo Roy <rshuvo036@gmail.com>
2024-05-20 14:46:59 +0600

    STEP #10.Added more comments and maked more naming improvements.

--- StudentList.java
+++ StudentList.java
@@ -2,66 +2,67 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
+	//main method
 	public static void main(String[] args) {
+		//Check argument
 		if(args.length != 1){
 			wrongArgument();
-			return;
+			return;	//exit early
 		}
-		String readLine = LoadData(Constants.STUDENT_LIST);
+		//Read StudentList
+		String studentList = loadStudents(Constants.STUDENT_LIST);
 		System.out.println(Constants.Loading);
+		//print all student
 		if(args[0].equals(Constants.ShowAll)) {
-			try {
-				String words[] = readLine.split(Constants.SplitAt);
-				for(String word : words) {
-					System.out.println(word);
-				}
-			} catch (Exception e){}
+			String students[] = studentList.split(Constants.SplitAt);
+			for(String student : students) {
+				System.out.println(student);
+			}
 		}
 		// return random student
 		else if(args[0].equals(Constants.ShowRandom)) {
-			try {
-				String words[] = readLine.split(Constants.SplitAt);
-				Random random = new Random();
-				int randomNumber = random.nextInt(words.length);
-				System.out.println(words[randomNumber]);
-			} catch (Exception e){}
+			String students[] = studentList.split(Constants.SplitAt);
+			Random random = new Random();
+			//generate random index of students
+			int randomIndex = random.nextInt(students.length);
+			System.out.println(students[randomIndex]);
 		}
+		//add new student
 		else if(args[0].contains(Constants.AddEntry)){
 			try {
-				BufferedWriter fileReader = new BufferedWriter(
+				BufferedWriter students = new BufferedWriter(
 											new FileWriter(Constants.STUDENT_LIST, true));
 				String newWord = args[0].substring(1);
 				DateFormat dateFormat = new SimpleDateFormat(Constants.Date_Format);
 				String fullDate = dateFormat.format(new Date());
-				fileReader.write(", "+newWord+"\nList last updated on "+fullDate);
-				fileReader.close();
+				students.write(", "+newWord+"\nList last updated on "+fullDate);
+				students.close();
 			} catch (Exception e){}
 		}
+		//find any student
 		else if(args[0].contains(Constants.FindEntry)) {
-			try {
-				String words[] = readLine.split(Constants.SplitAt);
-				boolean done = false;
-				String newWord = args[0].substring(1);
-				for(int idx = 0; idx<words.length && !done; idx++) {
-					if(words[idx].equals(newWord)) {
-						System.out.println(Constants.Found_Massage);
-						done=true;
-					}
+			String students[] = studentList.split(Constants.SplitAt);
+			boolean done = false;
+			String targetStudent = args[0].substring(1);
+			for(int idx = 0; idx<students.length && !done; idx++) {
+				if(students[idx].equals(targetStudent)) {
+					System.out.println(Constants.Found_Massage);
+					done=true;
 				}
-			} catch (Exception e){}
+			}
 		}
+		//count number of students
 		else if(args[0].contains(Constants.ShowCount)) {
-			try {
-				String words[] = readLine.split(Constants.SplitAt);
-				System.out.println(words.length +" word(s) found ");
-			} catch (Exception e){}
+			String students[] = studentList.split(Constants.SplitAt);
+			System.out.println(students.length +" student(s) found ");
 		}
+		//for wrong argument
 		else{
 			wrongArgument();
 		}
 		System.out.println(Constants.Loaded);
 	}
-	public static String LoadData(String fileName){
+	public static String loadStudents(String fileName){
 		try{
 			BufferedReader fileReader = new BufferedReader(
 										new InputStreamReader(
